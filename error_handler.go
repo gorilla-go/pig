5c0aac66a494922848f26599de719fb14384731c
叶华 <799475742@qq.com>
2024-01-13 23:20:01 +0800

    add error log

--- error_handler.go
+++ error_handler.go
@@ -17,7 +17,9 @@ func (e ErrorHandler) Handle(err any, context *Context) {
 	context.ResponseWriter().WriteHeader(500)
 	errno := fmt.Sprintf("%v", err)
 	context.Logger().Warning(errno)
-	_, err = context.ResponseWriter().Write(debug.Stack())
+	_, err = context.ResponseWriter().Write(
+		[]byte(fmt.Sprintf("Error: %s\n\r%s", errno, debug.Stack())),
+	)
 	if err != nil {
 		panic(err)
 	}
