5fae95adcfb9840638e7c81a271f2946971c74ee
叶华 <799475742@qq.com>
2024-01-14 11:33:49 +0800

    update

--- test/test.go
+++ test/test.go
@@ -33,6 +33,13 @@ func main() {
 		})
 	})
 
+	r.GET("/:id", func(c *pig.Context) {
+		postId := c.ParamVar().TrimString("id", "")
+		c.Json(map[string]interface{}{
+			"post_id": postId,
+		})
+	})
+
 	r.GET("/test/<id:[a-z]+>", func(context *pig.Context) {
 		context.Json(map[string]interface{}{
 			"test": context.ParamVar().TrimString("id", ""),
