a58e1c52d657836b4b92bff0164de7f224b42ab7
叶华 <799475742@qq.com>
2024-01-18 11:59:35 +0800

    update

--- test/test.go
+++ test/test.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	r := pig.NewRouter()
 	r.GET("/", func(context *pig.Context) {
-		context.Response().Echo("Hello World!")
+		context.Response().Echo("Hello World")
 	})
 
 	pig.New().Pprof().Router(r).Run(8848)
